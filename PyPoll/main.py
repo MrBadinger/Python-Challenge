f87b8668fb5bd9743d9aa91697cc39ff8cffd6fb
Michael Badinger <mcbadinger@gmail.com>
2020-10-04 21:40:32 -0500

    saving work

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,7 @@ import os
 import csv
 import shutil
 
+
 # Path to collect data from the Resources folder
 # ~Be extreamly careful where you are in Terminal~
 # ~If you are in the wrong spot of Terminal the file can not be read~
@@ -15,10 +16,11 @@ poll_csv = os.path.join("Resources", "test_election_data.csv")   #<-- Test data 
 # Variables
 total_votes = 0
 
-
+x = 0
 
 # List
 candidates = []
+votes = []
 
 #Dictionary
 election = {}
@@ -40,25 +42,46 @@ with open(poll_csv, 'r') as csv_file:
         if row[0] != (None, ""):
             total_votes = total_votes + 1
 
-        # Reviews Candidate column of csv and adds uniqui values to candidates list
+        # Reviews Candidate column of csv and adds unique values to candidates list
         if row[2] not in candidates:
             candidates.append(row[2])
+
+        # Adds vote into a list
+        votes.append(row[2])
     
-    
 
-    # Sets the list of candidates and makes each one a key in the election dictionary
-    for key in candidates:
-        election[key] = ""
+        # Sets the list of candidates and makes each one a key in the election dictionary
+        for key in candidates:
+            election[key] = 0 
+        
 
 
-print(candidates)
-print(candidates[0])
+        # if election[key] == row[2]:
+        #     print(election[key])
+        # for vote in votes:
+            # if election[key] == vote:
+            #     election[key] = x + 1
+
+
+print(election)
+print(votes)
+
+for vote in votes:
+    if vote == election[key]:
+        election[key].append(+1)
+
+
+
+# print(candidates)
+# print(candidates[0])
+print(election)
+print(votes)
 
 print("---------------------")
 print("                     ")
 
 print(election)
-
+print(election["Khan"])
 print("                     ")
 print("---------------------")
 
